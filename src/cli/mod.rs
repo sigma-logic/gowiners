6595a85510f6a9a5bdf3bc9f6f961b2264a50186
Danil Karpenko <limpix31@gmail.com>
2025-02-04 19:50:34 +0100

    feat: add separate syn command to cli

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,11 +3,12 @@ use std::{env::current_dir, fs, io::ErrorKind};
 use anyhow::anyhow;
 use bpaf::*;
 use colored::Colorize;
-
+use crate::pipeline::{EvaluationError, Pipeline};
 use crate::wrapper::{eda::GowinEda, pipeline::commands, project::Project};
 
 #[derive(Debug, Clone)]
 pub enum Opts {
+	Syn {},
 	Impl {},
 	Flash { preset: String },
 }
@@ -17,7 +18,10 @@ fn options() -> OptionParser<Opts> {
 		.to_options()
 		.descr("Runs synthesis and routing");
 
+	let syn_opts = construct!(Opts::Syn {}).to_options().descr("Runs synthesis");
+
 	let impl_cmd = impl_opts.command("impl");
+	let syn_cmd = syn_opts.command("syn");
 
 	let preset = positional::<String>("PRESET");
 
@@ -27,7 +31,7 @@ fn options() -> OptionParser<Opts> {
 
 	let flash_cmd = flash_opts.command("flash");
 
-	construct!([impl_cmd, flash_cmd])
+	construct!([impl_cmd, syn_cmd, flash_cmd])
 		.to_options()
 		.descr(env!("CARGO_PKG_DESCRIPTION"))
 }
@@ -50,15 +54,26 @@ pub fn run() -> anyhow::Result<()> {
 
 	let project: Project = toml::from_str(&gowiners_toml_str)?;
 
+	let configure_project = || -> Result<Pipeline, EvaluationError> {
+		println!("{}", "Run Gowin EDA".bold());
+		let mut wrk = eda.pipeline();
+		println!("  {}", "Configuring project".bold());
+		wrk.configure(&project)?;
+		Ok(wrk)
+	};
+
 	let run_opts = || -> anyhow::Result<()> {
 		match opts {
 			Opts::Impl { .. } => {
-				println!("{}", "Run implementation".bold());
-				let mut wrk = eda.pipeline();
-				println!("  {}", "Configuring project".bold());
-				wrk.configure(&project)?;
-				println!("  {}", "Run Gowin EDA syn and pnr tasks".bold());
+				let mut wrk = configure_project()?;
+				println!("  {}", "Run Implementation (Syn + PnR)".bold());
 				wrk.push(commands::Run::All);
+				wrk.run()?;
+			}
+			Opts::Syn { .. } => {
+				let mut wrk = configure_project()?;
+				println!("  {}", "Run Synthesis".bold());
+				wrk.push(commands::Run::Syn);
 				wrk.run()?;
 			}
 			Opts::Flash { preset } => {
